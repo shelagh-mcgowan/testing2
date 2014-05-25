df28d394d5a12a8a1901865f750f7a48954dc877
bellaincanto <shelagh_mcgowan@my.uri.edu>
2014-05-25 16:54:35 -0400

    Initial Commit

---
+++ testing try2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  testing try2
+//
+//  Created by Shelagh McGowan on 5/25/14.
+//  Copyright (c) 2014 Shelagh McGowan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ testing try2Tests/testing_try2Tests.m
@@ -0,0 +1,34 @@
+//
+//  testing_try2Tests.m
+//  testing try2Tests
+//
+//  Created by Shelagh McGowan on 5/25/14.
+//  Copyright (c) 2014 Shelagh McGowan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface testing_try2Tests : XCTestCase
+
+@end
+
+@implementation testing_try2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
